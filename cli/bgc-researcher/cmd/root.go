28e8ded37103e645518b294cd9b4d9908c12aa0f
Rob Holland <rob@temporal.io>
2021-11-10 17:49:45 +0000

    No need for config files for now.

--- cli/bgc-researcher/cmd/root.go
+++ cli/bgc-researcher/cmd/root.go
@@ -16,15 +16,8 @@ limitations under the License.
 package cmd
 
 import (
-	"fmt"
-	"os"
 	"github.com/spf13/cobra"
-
-	homedir "github.com/mitchellh/go-homedir"
-	"github.com/spf13/viper"
 )
-
-var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -45,41 +38,4 @@ to quickly create a Cobra application.`,
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	cobra.CheckErr(rootCmd.Execute())
-}
-
-func init() {
-	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.bgc-researcher.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		cobra.CheckErr(err)
-
-		// Search config in home directory with name ".bgc-researcher" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".bgc-researcher")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,8 @@ module github.com/temporalio/background-checks
 go 1.16
 
 require (
+	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/gorilla/mux v1.8.0
-	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.2.1
-	github.com/spf13/viper v1.9.0
 	go.temporal.io/sdk v1.11.0
 )
