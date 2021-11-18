49c01550e87f3ef2defd2ca361424ddce10c0d85
Rob Holland <rob@temporal.io>
2021-11-18 17:32:25 +0000

    Use tctl via docker compose.
    This avoids the user needing to have it locally.

--- create-search-attributes.sh
+++ create-search-attributes.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-tctl admin cluster add-search-attributes --name CandidateEmail --type String
-tctl admin cluster add-search-attributes --name BackgroundCheckStatus --type keyword
+docker compose exec temporal-admin-tools tctl admin cluster add-search-attributes --name CandidateEmail --type Text
+docker compose exec temporal-admin-tools tctl admin cluster add-search-attributes --name BackgroundCheckStatus --type keyword
