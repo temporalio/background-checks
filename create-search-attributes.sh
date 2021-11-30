97ad7bbedcf16f5215c089c371422845177c9727
Rob Holland <rob@temporal.io>
2021-11-30 17:02:11 +0000

    Remove redundant script.

--- create-search-attributes.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker compose exec temporal-admin-tools tctl admin cluster add-search-attributes --name CandidateEmail --type Text
-docker compose exec temporal-admin-tools tctl admin cluster add-search-attributes --name BackgroundCheckStatus --type keyword
