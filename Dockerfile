a30bf243cb9dff9caa0ef2cc6a6325a3c0b63e90
Rob Holland <rob@temporal.io>
2021-12-01 15:46:52 +0000

    Encryption via dataconverter.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
-FROM golang:1.17
+FROM golang:1.17 AS base
 
 WORKDIR /go/src/background-checks
+
+COPY --from=temporalio/admin-tools /usr/local/bin/tctl /usr/local/bin/tctl
 
 COPY go.mod go.sum ./
 
@@ -15,7 +17,11 @@ COPY mocks ./mocks/
 COPY queries ./queries/
 COPY signals ./signals/
 COPY types ./types/
+COPY temporal ./temporal
 COPY workflows ./workflows/
+
+RUN go install -v ./cli/bgc-dataconverter-plugin
+ENV TEMPORAL_CLI_PLUGIN_DATA_CONVERTER=bgc-dataconverter-plugin
 
 RUN go install -v ./cli/bgc-backend
 RUN go install -v ./cli/bgc-company
--- temporal/client.go
+++ temporal/client.go
@@ -3,13 +3,22 @@ package temporal
 import (
 	"os"
 
+	"github.com/temporalio/background-checks/temporal/dataconverter"
 	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/converter"
+	"go.temporal.io/sdk/workflow"
 )
 
 func NewClient(options client.Options) (client.Client, error) {
 	if options.HostPort == "" {
 		options.HostPort = os.Getenv("TEMPORAL_GRPC_ENDPOINT")
 	}
+
+	options.DataConverter = dataconverter.NewEncryptionDataConverter(
+		converter.GetDefaultDataConverter(),
+		dataconverter.DataConverterOptions{},
+	)
+	options.ContextPropagators = []workflow.ContextPropagator{dataconverter.NewContextPropagator()}
 
 	return client.NewClient(options)
 }
