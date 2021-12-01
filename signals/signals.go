f515f52f3db841fdc2e0a58b96d1ecc3d94b2aa2
Rob Holland <rob@temporal.io>
2021-12-01 08:16:40 +0000

    Tidy up unused signals.

--- signals/signals.go
+++ signals/signals.go
@@ -1,16 +1,6 @@
 package signals
 
 const (
-	BackgroundCheckStatus = "background-check-status"
-
-	AcceptRequest    = "accept-request"
-	AcceptSubmission = "accept-submission"
-	AcceptResponse   = "accept-response"
-
-	EmploymentVerificationRequest    = "employment-verification-request"
+	AcceptSubmission                 = "accept-submission"
 	EmploymentVerificationSubmission = "employment-verification-submission"
-	EmploymentVerificationResponse   = "employment-verification-response"
-
-	ResearcherTodoCreate   = "researcher-todo-create"
-	ResearcherTodoComplete = "researcher-todo-complete"
 )
