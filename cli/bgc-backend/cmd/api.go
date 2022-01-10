96bf0c87e5b1494520eabf8594bb6ed4df40a4c9
Rob Holland <rob@temporal.io>
2022-01-10 11:48:45 +0000

    Signal handling for server shutdown.

--- cli/bgc-backend/cmd/api.go
+++ cli/bgc-backend/cmd/api.go
@@ -3,6 +3,8 @@ package cmd
 import (
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 
 	"github.com/spf13/cobra"
 	"github.com/temporalio/background-checks/api"
@@ -26,7 +28,18 @@ var apiCmd = &cobra.Command{
 			Addr:    "0.0.0.0:8081",
 		}
 
-		log.Fatal(srv.ListenAndServe())
+		errCh := make(chan error, 1)
+		go func() { errCh <- srv.ListenAndServe() }()
+
+		sigCh := make(chan os.Signal, 1)
+		signal.Notify(sigCh, os.Interrupt)
+
+		select {
+		case <-sigCh:
+			srv.Close()
+		case err = <-errCh:
+			log.Fatalf("error: %v", err)
+		}
 	},
 }
 
