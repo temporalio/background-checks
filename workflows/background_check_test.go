631dc729f01aaac0bac3add83409a119c73580f9
Rob Holland <rob@temporal.io>
2022-01-11 14:53:12 +0000

    Add a test for full package.

--- workflows/background_check_test.go
+++ workflows/background_check_test.go
@@ -12,7 +12,7 @@ import (
 	"go.temporal.io/sdk/workflow"
 )
 
-func TestBackgroundCheckWorkflowComplete(t *testing.T) {
+func TestBackgroundCheckWorkflowStandard(t *testing.T) {
 	s := testsuite.WorkflowTestSuite{}
 	env := s.NewTestWorkflowEnvironment()
 	a := activities.Activities{SMTPStub: true, HTTPStub: true}
@@ -23,8 +23,6 @@ func TestBackgroundCheckWorkflowComplete(t *testing.T) {
 	env.RegisterActivity(a.SSNTrace)
 	env.RegisterWorkflow(workflows.FederalCriminalSearch)
 	env.RegisterActivity(a.FederalCriminalSearch)
-	env.RegisterActivity(a.StateCriminalSearch)
-	env.RegisterActivity(a.SendEmploymentVerificationRequestEmail)
 	env.RegisterActivity(a.SendReportEmail)
 
 	details := types.CandidateDetails{
@@ -45,6 +43,47 @@ func TestBackgroundCheckWorkflowComplete(t *testing.T) {
 	})
 
 	env.ExecuteWorkflow(workflows.BackgroundCheck, &types.BackgroundCheckWorkflowInput{Email: "john@example.com", Package: "standard"})
+
+	err := env.GetWorkflowError()
+	assert.NoError(t, err)
+}
+
+func TestBackgroundCheckWorkflowFull(t *testing.T) {
+	s := testsuite.WorkflowTestSuite{}
+	env := s.NewTestWorkflowEnvironment()
+	a := activities.Activities{SMTPStub: true, HTTPStub: true}
+
+	env.RegisterWorkflow(workflows.Accept)
+	env.RegisterActivity(a.SendAcceptEmail)
+	env.RegisterWorkflow(workflows.SSNTrace)
+	env.RegisterActivity(a.SSNTrace)
+	env.RegisterWorkflow(workflows.FederalCriminalSearch)
+	env.RegisterActivity(a.FederalCriminalSearch)
+	env.RegisterWorkflow(workflows.StateCriminalSearch)
+	env.RegisterActivity(a.StateCriminalSearch)
+	env.RegisterWorkflow(workflows.MotorVehicleIncidentSearch)
+	env.RegisterWorkflow(workflows.EmploymentVerification)
+	env.RegisterActivity(a.SendEmploymentVerificationRequestEmail)
+	env.RegisterActivity(a.SendReportEmail)
+
+	details := types.CandidateDetails{
+		FullName: "John Smith",
+		SSN:      "111-11-1111",
+		DOB:      "1981-01-01",
+		Address:  "1 Chestnut Avenue",
+	}
+
+	env.SetOnChildWorkflowStartedListener(func(workflowInfo *workflow.Info, ctx workflow.Context, args converter.EncodedValues) {
+		if workflowInfo.WorkflowExecution.ID == workflows.AcceptWorkflowID("john@example.com") {
+			env.SignalWorkflowByID(
+				workflows.AcceptWorkflowID("john@example.com"),
+				workflows.AcceptSubmissionSignal,
+				types.AcceptSubmissionSignal{Accepted: true, CandidateDetails: details},
+			)
+		}
+	})
+
+	env.ExecuteWorkflow(workflows.BackgroundCheck, &types.BackgroundCheckWorkflowInput{Email: "john@example.com", Package: "full"})
 
 	err := env.GetWorkflowError()
 	assert.NoError(t, err)
