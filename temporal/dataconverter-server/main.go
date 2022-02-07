08e1db235ed9992af3c25fef49e1167184933074
Rob Holland <rob@temporal.io>
2022-02-07 10:48:58 +0000

    Various dc fixes.

--- temporal/dataconverter-server/main.go
+++ temporal/dataconverter-server/main.go
@@ -1,21 +1,45 @@
 package main
 
 import (
+	_ "embed"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"text/template"
 
 	"github.com/temporalio/background-checks/temporal/dataconverter"
 	"go.temporal.io/sdk/converter"
 )
 
+//go:embed iframe.go.html
+var iframeHTML string
+var iframeHTMLTemplate = template.Must(template.New("iframe").Parse(iframeHTML))
+
 func main() {
-	dc := dataconverter.Encoder{}
+	frontend := os.Args[1]
+
+	if frontend == "" {
+		log.Fatal("frontend URL argument is required")
+	}
+
+	http.HandleFunc("/js", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+
+		err := iframeHTMLTemplate.Execute(w, map[string]string{
+			"BasePath": r.URL.Path[0 : len(r.URL.Path)-3],
+			"Frontend": frontend,
+		})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	encoder := dataconverter.Encoder{}
+	http.Handle("/", converter.NewPayloadEncoderHTTPHandler(&encoder))
 
 	srv := &http.Server{
-		Addr:    "0.0.0.0:8081",
-		Handler: converter.NewPayloadEncoderHTTPHandler(&dc, os.Args[1]),
+		Addr: "0.0.0.0:8081",
 	}
 
 	errCh := make(chan error, 1)
